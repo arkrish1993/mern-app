4d2c14727263d949e0508cdf36c63f233e78a4d6
arkrish1993 <archanarkrishnan3@gmail.com>
2021-06-20 16:40:30 +0530

    Auth page switch to signup completed

--- project-MERN/frontend/src/user/pages/Auth.js
+++ project-MERN/frontend/src/user/pages/Auth.js
@@ -1,15 +1,20 @@
-import { useForm } from "../../hooks/form-hook";
-import Button from "../../shared/components/Form/Button";
-import Input from "../../shared/components/Form/Input";
+import React, { useState } from "react";
+
 import Card from "../../shared/components/UI/Card";
+import Input from "../../shared/components/Form/Input";
+import Button from "../../shared/components/Form/Button";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
+import { useForm } from "../../hooks/form-hook";
 import "./Auth.css";
 
-const Auth = (props) => {
-  const [formState, inputHandler] = useForm(
+const Auth = () => {
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
         value: "",
@@ -23,7 +28,31 @@ const Auth = (props) => {
     false
   );
 
-  const submitHandler = (event) => {
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: "",
+            isValid: false,
+          },
+        },
+        false
+      );
+    }
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+
+  const authSubmitHandler = (event) => {
     event.preventDefault();
     console.log(formState.inputs);
   };
@@ -32,29 +61,43 @@ const Auth = (props) => {
     <Card className="authentication">
       <h2>Login Required</h2>
       <hr />
-      <form onSubmit={submitHandler}>
+      <form onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            element="input"
+            id="name"
+            type="text"
+            label="Your Name"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a name."
+            onInput={inputHandler}
+          />
+        )}
         <Input
+          element="input"
           id="email"
           type="email"
-          label="E-mail Address"
-          element="input"
-          errorText="Please enter a valid email address."
+          label="E-Mail"
           validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address."
           onInput={inputHandler}
         />
         <Input
+          element="input"
           id="password"
           type="password"
           label="Password"
-          element="input"
-          errorText="The password should be at least 6 characters long."
-          validators={[VALIDATOR_MINLENGTH(6)]}
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid password, at least 5 characters."
           onInput={inputHandler}
         />
         <Button type="submit" disabled={!formState.isValid}>
-          LOGIN
+          {isLoginMode ? "LOGIN" : "SIGNUP"}
         </Button>
       </form>
+      <Button inverse onClick={switchModeHandler}>
+        SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
+      </Button>
     </Card>
   );
 };
